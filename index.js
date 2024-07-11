ddacb29a9aec19b841246d0978eb0006e48a8f71
Harry EMARH <emarh.harry.code@gmail.com>
2024-07-11 23:57:49 +0200

    Connection to MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT || 7011;
+const mongoUri = process.env.MONGO_URI; // Assurez-vous que cette variable est définie dans votre fichier .env
 
+// Connexion à MongoDB
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on the port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
